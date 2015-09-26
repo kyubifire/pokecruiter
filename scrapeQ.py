1b710452feb0c5bf070e347d01ab8c35e984162c
saNed <this.is.saned@gmail.com>
2015-09-26 18:07:23 -0500

    fixed scraping script

--- scrapeQ.py
+++ scrapeQ.py
@@ -5,7 +5,10 @@ page = requests.get('http://www.careerride.com/test.aspx?type=Data-structure')
 tree = html.fromstring(page.text)
 
 question = tree.xpath('//span[@class="question"]/text()')
-answers = tree.xpath('//span[@class="answer"]/text()')
+answers = tree.xpath('//label[@for="rblAnswer_0"]/text()')
+answers += tree.xpath('//label[@for="rblAnswer_1"]/text()')
+answers += tree.xpath('//label[@for="rblAnswer_2"]/text()')
+answers += tree.xpath('//label[@for="rblAnswer_3"]/text()')
 
 print(question)
 print(answers)
