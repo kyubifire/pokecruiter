fee8903fadca6dac61c883f2366366f76a2024f8
FronTexas <fahrankamili@wireless-10-145-193-21.public.utexas.edu>
2015-09-26 22:46:54 -0500

    Adding entry animation for the player

--- app/src/main/java/com/example/nilo/pokecruiter/MainActivity.java
+++ app/src/main/java/com/example/nilo/pokecruiter/MainActivity.java
@@ -2,22 +2,62 @@ package com.example.nilo.pokecruiter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.Display;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tvQuestion = (TextView) findViewById(R.id.tvQuestion);
-        tvQuestion.setMovementMethod(new ScrollingMovementMethod());
+
+        player = (ImageView) findViewById(R.id.player);
+
+        slideRight(player);
 
     }
 
+    private void slideRight(View view) {
+        Display display = getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
 
+        Animation animationSlideInLeft = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        animationSlideInLeft.setDuration(1000);
+        animationSlideInLeft.setAnimationListener(animationSlideInLeftListener);
+
+        view.startAnimation(animationSlideInLeft);
+        view.setVisibility(View.VISIBLE);
+
+//        final Animation posX = new TranslateAnimation(0,width-50,0,0);
+//        posX.setDuration(500);
+//        posX.setFillAfter(true);
+//
+//        view.startAnimation(posX);
+//        view.setVisibility(View.VISIBLE);
+    }
+
+    Animation.AnimationListener animationSlideInLeftListener =
+            new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            };
 
 }
