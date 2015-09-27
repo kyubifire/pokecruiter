8a138090a94723b458388d8bfb8706a13db3663a
saNed <this.is.saned@gmail.com>
2015-09-27 08:37:01 -0500

    added more questions

---
+++ scrape_moreQ.py
@@ -0,0 +1,35 @@
+#This script takes the question/answer pairs from the below website and parses
+#  them into a json
+
+# The last entry shouldn't print a comma, I've been removing it by hand
+
+from lxml import html
+import requests
+import json
+
+f = open('datastructureQs_extra.json', 'w')
+f.write("{\n")
+
+for i in range(1,42):
+	page = requests.get('http://www.careerride.com/question-' + str(i) + '-C')
+	tree = html.fromstring(page.text)
+
+	question = tree.xpath('//span[@class="question"]/text()')
+	answers = tree.xpath('//span[@class="answer"]/text()')
+	answers = [x[3:] for x in answers]
+
+	answers[-1] = answers[-1][16:]
+	# for i, answer in enumerate(answers):
+	# 	if answer == answers[-1][16:]:
+	# 		answers += str(i)
+	# del answers[-2]
+	print(question)
+	print(answers)
+	if len(max(answers, key=len)) <= 20:
+		answers = json.dumps(answers)
+		print("THIS IS GOING TO PRINT")
+		f.write('    "(working in C) ' + str(question[0]) + '":\n        ' + str(answers) + ",\n")
+	else:
+		print("8888888888")
+
+f.write("}")
