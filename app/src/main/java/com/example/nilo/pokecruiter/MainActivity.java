6d378c0dcbcf99a912f10552ee8cb1061d6b0475
FronTexas <fahrankamili@wireless-10-145-193-21.public.utexas.edu>
2015-09-27 00:59:19 -0500

    Working on question io

--- app/src/main/java/com/example/nilo/pokecruiter/MainActivity.java
+++ app/src/main/java/com/example/nilo/pokecruiter/MainActivity.java
@@ -1,45 +1,95 @@
 package com.example.nilo.pokecruiter;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView player;
+    private ImageView enemy;
+    private JSONObject jsonObject;
+    private Iterator<?> questions;
+    private TextView tvQuestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         player = (ImageView) findViewById(R.id.player);
+        enemy = (ImageView) findViewById(R.id.enemy);
+        tvQuestion = (TextView) findViewById(R.id.tvQuestion);
 
         slideRight(player);
 
+        slideRight(enemy);
+        try {
+            readQuestionAndAnswer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            showQuestionAndAnswer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void readQuestionAndAnswer()throws IOException  {
+        AssetManager am = getAssets();
+        InputStream is = am.open("datastructureQs.json");
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder builder = new StringBuilder();
+        String line;
+        while((line = bReader.readLine())!=null){
+            builder.append(line);
+        }
+
+        try {
+            jsonObject = new JSONObject(builder.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        questions = jsonObject.keys();
+    }
+
+    private void showQuestionAndAnswer() throws IOException, JSONException {
+        if(questions.hasNext()){
+            String question = (String) questions.next();
+            JSONArray answers = jsonObject.getJSONArray(question);
+
+            tvQuestion.setText(question);
+
+
+        }
     }
 
     private void slideRight(View view) {
-        Display display = getWindowManager().getDefaultDisplay();
-        int width = display.getWidth();
-
         Animation animationSlideInLeft = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
         animationSlideInLeft.setDuration(1000);
         animationSlideInLeft.setAnimationListener(animationSlideInLeftListener);
 
         view.startAnimation(animationSlideInLeft);
         view.setVisibility(View.VISIBLE);
-
-//        final Animation posX = new TranslateAnimation(0,width-50,0,0);
-//        posX.setDuration(500);
-//        posX.setFillAfter(true);
-//
-//        view.startAnimation(posX);
-//        view.setVisibility(View.VISIBLE);
     }
 
     Animation.AnimationListener animationSlideInLeftListener =
